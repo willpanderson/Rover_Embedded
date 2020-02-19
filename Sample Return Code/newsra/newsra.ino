47f683604972ece405eb008599f2b0172b7ecb91
K-no4 <waabee.geshow@gmail.com>
2020-02-19 10:53:34 -0600

    Added hardware interupt

--- Sample Return Code/newsra/newsra.ino
+++ Sample Return Code/newsra/newsra.ino
@@ -1,16 +1,16 @@
 #include <Wire.h>
 
 /*
- * 
- * 
+ *
+ *
  * IN DEVELOPMENT, THIS WILL NOT WORK WHATSOEVER
- * General layout for continuing further 
- * 
+ * General layout for continuing further
+ *
  * -EMBEDDED TEAM
  */
 // Drill Motor (Use OUT1 and OUT2 on  MotorDriver "A")
-#define InputA_Drill 30 
-#define InputB_Drill 31 
+#define InputA_Drill 30
+#define InputB_Drill 31
 
 // Elevator Motor (Use OUT3 and OUT4 on  MotorDriver "A")
 #define InputA_Elevator 32
@@ -26,25 +26,45 @@
 #define Enable_Solenoid 37
 
 // Upper Limit Switch
-#define UpperSwitch 3 
+#define UpperSwitch 3
+
 
 // Lower Limit Switch
 #define LowerSwitch 2
 
-// I2C Data Lines 
+// I2C Data Lines
 #define SDA 20
 #define SCL 21
 
 
+void setup()
+{
+  Serial.begin(9600);
 
+  pinMode(InputA_Bin, OUTPUT);
+  pinMode(InputB_Bin, OUTPUT);
+  pinMode(InputA_Drill, OUTPUT);
+  pinMode(InputB_Drill, OUTPUT);
+  pinMode(InputA_Elevator, OUTPUT);
+  pinMode(InputB_Elevator, OUTPUT);
+  pinMode(Enable_Solenoid, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(UpperSwitch),blink,CHANGE); //hardware interupt for upper switch
+  attachInterrupt(digitalPinToInterrupt(LowerSwitch),blink,CHANGE); // hardware interupt for lower switch
+}
 
+void blink(){
+  stop(); // stops elevator when switch is active
+}
 
+void Stop(){
+  digitalWrite(InputA_Elevator,LOW);
+  digitalWrite(InputB_Elevator,LOW);
+}
 
 
 // need to place a data parsing function for the I2C line
 
 
-//need to place a hardware interrupt fuction 
 
 
 int BinByte;  // New Command for Bin Change after Data Parsing
@@ -58,7 +78,7 @@ int DownLimitSwitch = 1; // Lower Limit Switch for Elevator
 
 
 /*
-  The following preprocessor definitions are values that 
+  The following preprocessor definitions are values that
   are used to reset the Sample Return Module into its home
   state when the unit is powered up or has a unexpected power
   loss.
@@ -67,7 +87,7 @@ int DownLimitSwitch = 1; // Lower Limit Switch for Elevator
   in the up position, the bin will be returned to the 1st
   position, and the servo will be in its closed state.
 
-  Preprocessors are used due to memory constraints with the 
+  Preprocessors are used due to memory constraints with the
   AT2560 processsor.
 
 */
@@ -118,17 +138,17 @@ void DrillControl(int i,int j)
 
 }
 /*
- The operation of the bin requires an algorithm to determine the least amount of 
- bins needed to pass in order to get from Bin A to Bin B. 
- 
- The function also preforms a check on the current status of the liquid 
+ The operation of the bin requires an algorithm to determine the least amount of
+ bins needed to pass in order to get from Bin A to Bin B.
+
+ The function also preforms a check on the current status of the liquid
  container servo to determine if is closed or not. If the servo is in its open
- state, ServoControl() will be called to close the servo and recursivley call 
+ state, ServoControl() will be called to close the servo and recursivley call
  BinControl() again.If the servo is in its closed state, the servo will find
- the mimimum amount of bin changes required to 
+ the mimimum amount of bin changes required to
 
 
- 
+
  */
 void BinControl(int i,int j)
 {
@@ -141,7 +161,7 @@ void BinControl(int i,int j)
   }
   if (j == 0)
   {
-    
+
     if (forward < backward)
     {
     //turn gear left
